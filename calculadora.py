77808bc8a302c55a9f95b49101149d3ce13cd344
alejo_varela <chaconalejandrovarela@gmail.com>
2026-09-03 13:52:14 -0500

    feat: agregar operacion de division, potenciacion  y factorizacion

--- calculadora.py
+++ calculadora.py
@@ -3,8 +3,32 @@
 def sumar(a, b):
     return a + b
 
+
 def restar(a, b):
     return a - b
 
+
 def multiplicar(a, b):
     return a * b
+
+
+def dividir(a, b):
+    if b == 0:
+        return "Error: no se puede dividir entre cero"
+    return a / b
+
+
+def potenciar(a, b):
+    return a ** b
+
+
+def factorial(n):
+    if n < 0:
+        return "Error: el factorial no existe para números negativos"
+
+    resultado = 1
+
+    for i in range(1, n + 1):
+        resultado *= i
+
+    return resultado
